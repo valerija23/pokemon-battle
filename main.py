67f27b833cb9558342c57336f402d30715bafc78
Valerija Januskevica <you@example.com>
2024-03-06 10:12:55 +0200

    .

--- main.py
+++ main.py
@@ -24,7 +24,9 @@ pokemons_file = open('pokemons.json') # opening JSON file
 pokemons = json.load(pokemons_file) # returns JSON object as a dictionary
 pokemons_file.close() # Closing file
 
-print(pokemons[0])
+print(pokemons[0]["total"])
+print(pokemons[0]["hp"])
+print(pokemons[0]["attack"])
 
 while True:
     print("1. Show pokemon by index")
@@ -36,15 +38,27 @@ while True:
     choice = input("Enter your choice (1-5): ")
 
     if choice == '1':
+        number = int(input("Show Pokemon by index"))   
+        print(pokemons[number - 1])
         # https://www.w3schools.com/python/python_dictionaries_access.asp
         pass
     elif choice == '2':
+        # {'number': 4, 'name': 'Charmander', 'type_1': 'Fire', 'type_2': '', 'total': 309, 'hp': 39, 'attack': 52, 'defense': 43}
+        def strongest_pokemon(pokemon):
+            return int(pokemon["total"])
+        pokemons.sort(key = strongest_pokemon, reverse = True)
+        print(pokemons[0:10])
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '3':
+        def weakest_pokemon(pokemon):
+            return int(pokemon["total"])
+        pokemons.sort(key = weakest_pokemon)
+        print(pokemons[0:10])
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '4':
+        
         # Battle
         # 
         # https://www.w3schools.com/python/ref_random_choice.asp - random choice
